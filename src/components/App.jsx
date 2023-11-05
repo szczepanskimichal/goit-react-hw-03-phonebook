50090058d57dfed4581023a69f132401e2491488
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-05 19:45:02 +0100

    cd...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,123 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import React from 'react';
+import './App.css';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+
+// comment to deletedsdsd
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  constructor() {
+    super();
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    this.state.contacts = parsedContacts || [];
+  }
+
+  addNewContact = event => {
+    event.preventDefault();
+
+    const nameValue = event.target.elements.name.value;
+    const numberValue = event.target.elements.number.value;
+    const namePattern = new RegExp(event.target.elements.name.pattern);
+    const numberPattern = new RegExp(event.target.elements.number.pattern);
+
+    const newContact = {
+      id: nanoid(),
+      name: event.target.elements.name.value,
+      number: event.target.elements.number.value,
+    };
+
+    const isNameValid = namePattern.test(nameValue);
+    const isNumberValid = numberPattern.test(numberValue);
+
+    let errorMessage = '';
+
+    if (!isNameValid) {
+      errorMessage += 'Invalid name input. ';
+    }
+
+    if (!isNumberValid) {
+      errorMessage += 'Invalid number input.';
+    }
+
+    if (errorMessage) {
+      alert(errorMessage);
+      return;
+    }
+
+    if (
+      this.state.contacts.some(
+        contact =>
+          contact.name.toLowerCase() === newContact.name.toLowerCase() ||
+          contact.number.toLowerCase() === newContact.number.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+      event.target.reset();
+    }
+  };
+
+  deleteContact = idToDelete => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== idToDelete),
+    }));
+  };
+
+  setStateInput = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  filterContacts = event => {
+    this.setState({
+      filter: event.target.value,
+    });
+  };
+
+  renderContacts = () => {
+    const { filter, contacts } = this.state;
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return filteredContacts.map(contact => (
+      <li key={contact.id}>
+        {contact.name}: {contact.number}
+        <button onClick={() => this.deleteContact(contact.id)}>delete</button>
+      </li>
+    ));
+  };
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <ContactForm
+          submit={this.addNewContact}
+          contacts={this.state.contacts}
+        />
+        <ContactList list={this.renderContacts()}>
+          <Filter filteredContacts={this.filterContacts} />
+        </ContactList>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,37 @@
+import css from './ContactForm.module.css';
+
+const ContactForm = ({ submit, contacts }) => {
+  const { name, number } = contacts;
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <form onSubmit={submit} className={css.contact}>
+        <span>Name:</span>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Z]+( [a-zA-Z]+)?$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          placeholder="Type name as John Doe"
+        />
+        <span>Number:</span>
+        <input
+          type="tel"
+          name="number"
+          pattern="^\d{1,3}([- ]?\d{1,3}){2,3}$"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          placeholder="Type number as 000-00-00"
+        />
+        <button type="submit" className={css.contact__button}>
+          Add contact
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
